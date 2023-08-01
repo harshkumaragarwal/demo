2906522a90d05d3bf0e768ffc13851d46116c48b
harshkumaragarwal <89272576+harshkumaragarwal@users.noreply.github.com>
2023-08-01 21:09:38 +0530

    demo

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+
+
+function App() {
+  return (
+    <>
+      Hi
+      <ChakraProvider>
+        Hi
+      </ChakraProvider>
+    </>
+    //
+  );
+}
+
+export default App;
